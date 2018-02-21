52128af1fdba586be286f9c0f77c86e20d1f5f05
Ivan Solomichev <vanijatko.social@gmail.com>
2018-02-21 12:30:38 +0200

    Fixed tests
    1. renamed array properties to plural form;
    2. fixed beforeEach to reset arrays in the beginning;
    3. updated to use new method names.

--- test/gdp_crowdsale.js
+++ test/gdp_crowdsale.js
@@ -8,8 +8,8 @@ const Stage_length = 86400; // 1 day
 const CurrentTime = web3.eth.getBlock('latest').timestamp;
 
 const WalletAddr = web3.eth.accounts[9];
-let startTime = [];
-let endTime = [];
+let startTimes;
+let endTimes;
 
 /**
  * Week   0 1 ETH - 3000 - GDP 
@@ -17,7 +17,7 @@ let endTime = [];
  * Week 3&4 1 ETH - 2000 - GDP
  * Week 5&6 1 ETH - 1800 - GDP
  */
-const Rate = [3000, 2200, 2000, 1800];
+const Rates = [3000, 2200, 2000, 1800];
 
 contract('GDPCrowdsale', (accounts) => {
 
@@ -26,31 +26,36 @@ contract('GDPCrowdsale', (accounts) => {
   let crowdsale;
 
   beforeEach('reset', async () => {
+    startTimes = [];
+    endTimes = [];
 
     //  construct stages and rates
-    for (let i = 0; i < Rate.length; i++) {
+    for (let i = 0; i < Rates.length; i++) {
       if (i == 0) {
-        startTime.push(CurrentTime + 1);
-        endTime.push(CurrentTime + 1 + Stage_length);
+        startTimes.push(CurrentTime + 1);
+        endTimes.push(CurrentTime + 1 + Stage_length);
       } else {
-        startTime.push(endTime[i - 1] + 1);
-        endTime.push(startTime[i] + Stage_length);
+        startTimes.push(endTimes[i - 1] + 1);
+        endTimes.push(startTimes[i] + Stage_length);
       }
     }
 
-    crowdsale = await GDPCrowdsale.new(startTime, endTime, Rate, WalletAddr);
+    crowdsale = await GDPCrowdsale.new(startTimes, endTimes, Rates, WalletAddr);
   });
 
   describe('initial validation', () => {
     it('validate initial values', async () => {
-      assert.equal((await crowdsale.crowdsaleStagesCount.call()).toNumber(), Rate.length, 'wrong ICO stages count');
+      assert.equal((await crowdsale.stagesCount.call()).toNumber(), Rates.length, 'wrong ICO stages count');
       assert.notEqual(await crowdsale.token.call(), 0, 'token should be already created');
+      assert.isFalse(await crowdsale.hasEnded.call(), 'crowdsale should still go on');
     });
 
     it('validate newly created token', async () => {
-      let token = await GDPToken.at(await crowdsale.token.call());
+      let token = GDPToken.at(await crowdsale.token.call());
       assert.equal(await token.owner.call(), crowdsale.address, 'wrong token owner address');
     });
   });
+
+
 
 });
