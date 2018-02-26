a6b3b29f0d809a1759cef2711c2bb98196a81585
Ivan Solomichev <vanijatko.social@gmail.com>
2018-02-26 17:23:35 +0200

    Added Ropsten
    1. added Ropsten network
    2. added Infura. See key in email

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "actress gain faculty gloom lion eight merge gloom ginger horn inner foil"; //  IMPORTANT: keep in separate file
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -7,6 +10,15 @@ module.exports = {
       port: 8545,
       network_id: '*',
       gas: '4712388'
+    },
+    ropsten: {
+      // IMPORTANT: host / port or provider
+      // host: "localhost",
+      // port: 8545,
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/5bJFB4nwE3wJJgBHjnk8"),
+      network_id: 3,
+      gas: 4700000
+      // IMPORTANT: try from:
     }
   }
 };
