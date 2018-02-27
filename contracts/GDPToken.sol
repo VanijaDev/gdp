88d17226a57b2208fb652fc8db7b33e56fcf308b
Ivan Solomichev <vanijatko.social@gmail.com>
2018-02-27 15:31:28 +0200

    quotes updated

--- contracts/GDPToken.sol
+++ contracts/GDPToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.18;
 
-import "../node_modules/zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import '../node_modules/zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 
 contract GDPToken is MintableToken {
 
