a034c0878f8aea0ddf4587365cfdb1847a4757c4
Ivan Solomichev <vanijatko.social@gmail.com>
2018-03-01 13:22:53 +0200

    Improved scropt

--- test/helpers/latestTime.js
+++ test/helpers/latestTime.js
@@ -2,24 +2,3 @@
 exports.latestTime = function () {
     return web3.eth.getBlock('latest').timestamp;
 }
-
-function LatestTime(web3) {
-    this.latesTime = () => {
-        return new Promise((resolve, reject) => {
-            web3.currentProvider.sendAsync({
-                jsonrpc: '2.0',
-                method: 'get_block',
-                id: new Date().getTime(),
-                params: ['latest']
-            }, (err, result) => {
-                if (err) {
-                    return reject(err);
-                }
-
-                return resolve
-            });
-        });
-    };
-}
-
-module.exports = LatestTime;
