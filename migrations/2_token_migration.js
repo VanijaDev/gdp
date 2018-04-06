aa558cfcfe0341ff66249cb9063e3320aff38850
Ivan Solomichev <vanijatko.social@gmail.com>
2018-04-06 11:23:21 +0300

    improved migration file

--- migrations/2_token_migration.js
+++ migrations/2_token_migration.js
@@ -3,36 +3,36 @@ let GDPCrowdsale = artifacts.require("./GDPCrowdsale.sol");
 let IncreaseTime = require('../test/helpers/increaseTime');
 
 module.exports = function (deployer, network, accounts) {
+    /**
+     * PRODUCTION
+     */
     let BASIC_RATE = 1700;
     let BONUSES = [40, 30, 20, 10, 5]; //  in %
     let WALLET; //TODO: add wallet from GD
     let SOFT_CAP = 1000; // in ETH;
     let HARD_CAP = 35000; // in ETH;
-    /**
-     * @dev This is individual stage goals, not crowdsale parts
-     */
     let STAGE_GOALS = [2000, 5000, 5000, 5000, 5000]; // in ETH
-
     let start = 000000;
     let end = 000000;
 
-    /**
-     * TEST only
-     */
-    SOFT_CAP = 50;
-    HARD_CAP = 75;
-    STAGE_GOALS = [2, 5, 5, 5, 5];
-    WALLET = accounts[3];
-    start = 000000;
-    end = 000000;
-    /**
-     * TEST only
-     */
 
-
-    if (network != 'ropsten') {
+    /**
+     * TEST
+     */
+    if (network == 'develop') {
+        SOFT_CAP = 50;
+        HARD_CAP = 75;
+        STAGE_GOALS = [2, 5, 5, 5, 5];
+        WALLET = accounts[9];
         start = web3.eth.getBlock('latest').timestamp + 10;
         end = start + IncreaseTime.duration.days(5);
+    } else if (network == 'ropsten') {
+        SOFT_CAP = 5;
+        HARD_CAP = 10;
+        STAGE_GOALS = [1, 2, 2, 2, 2];
+        WALLET = 0x7be996203abfe93b9cf537d3c940f9092664b7c6; //     test wallet;
+        start = 000000000;
+        end = 00000000;
     }
 
     console.log('\nstart, end: ', start, end);
