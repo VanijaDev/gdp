8bc98147913a862e0ef69e71f1356b6a15df1c5e
Ivan Solomichev <vanijatko.social@gmail.com>
2018-04-11 16:14:04 +0300

    updated crowdsaleMock with new values

--- test/helpers/crowdsaleMock.js
+++ test/helpers/crowdsaleMock.js
@@ -1,7 +1,7 @@
 var mock = {
     rate: 1700,
-    stageBonuses: [40, 20, 5],
-    stageGoals: [2, 5, 5, 5, 5],
+    stageBonuses: [40, 30, 20, 10, 5],
+    stageGoals: [2, 5, 4, 6, 7],
     wallet: web3.eth.accounts[9],
     softCap: 50,
     hardCap: 75
