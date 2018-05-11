2424bcf393fdb0e73cb56aede8bb8d9d22feca94
Ivan Solomichev <vanijatko.social@gmail.com>
2018-05-11 19:39:44 +0300

    moved Infura credentials to separate file

--- truffle.js
+++ truffle.js
@@ -1,12 +1,6 @@
+const infura = require('./utils/infuraCreds.js');
+
 var HDWalletProvider = require("truffle-hdwallet-provider");
-
-/**
- * TODO
- * 1. client needs to create his own phrase
- * 2. keep in separate file
- */
-
-var mnemonic = "actress gain faculty gloom lion eight merge gloom ginger horn inner foil";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -21,7 +15,7 @@ module.exports = {
       // IMPORTANT: host / port or provider
       // host: "localhost",
       // port: 8545,
-      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/5bJFB4nwE3wJJgBHjnk8"),
+      provider: new HDWalletProvider(infura.mnemonic(), infura.url()),
       network_id: 3,
       gas: 4700000,
       // IMPORTANT: try from:
